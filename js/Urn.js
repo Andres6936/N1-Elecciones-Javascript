cec816cb50fbd27282a79371ecd08a8813a5e3fe
Joan Andrés <andres6936@live.com>
2019-12-13 15:26:08 -0500

    Added Buttons and Js

---
+++ js/Urn.js
@@ -0,0 +1,14 @@
+function vote (nameOfCandidate) {
+    if(nameOfCandidate === "Frank")
+    {
+        console.log("Frank");
+    }
+    else if (nameOfCandidate === "Claire")
+    {
+        console.log("Claire");
+    }
+    else if (nameOfCandidate === "Obama")
+    {
+        console.log("Obama");
+    }
+}
